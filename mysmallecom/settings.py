0410d79d92eeb89fb41b1d824d72cf6af424460f
AnkushP <ankushpokalwar777@gmail.com>
2020-01-23 11:03:02 +0530

    changed email settings

--- mysmallecom/settings.py
+++ mysmallecom/settings.py
@@ -144,5 +144,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ankushpokalwar777@gmail.com'
-EMAIL_HOST_PASSWORD = 'Ankush@123'
+EMAIL_HOST_USER = 'Your Emailid'
+EMAIL_HOST_PASSWORD = 'Your password'
